1e294e02ab479f217abffe6f9f98b3c67970736c
_Justrishi <rishimud@gmail.com>
2021-08-11 21:16:43 +0530

    Stage prepared for Game Dev
    All game prerequisites made

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeHeadSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeHeadSpriteRenderer.sprite = GameAssets.instance.snakeHeadSprite;
+    }
+}
