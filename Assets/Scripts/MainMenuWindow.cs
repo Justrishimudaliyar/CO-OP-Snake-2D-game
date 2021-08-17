8fb89170662505be0995f3d7db17bed1d7d117f9
_Justrishi <rishimud@gmail.com>
2021-08-17 20:01:42 +0530

    SinglePlayer Game finally made
    Co-OP remains

--- Assets/Scripts/MainMenuWindow.cs
+++ Assets/Scripts/MainMenuWindow.cs
@@ -9,20 +9,37 @@ public class MainMenuWindow : MonoBehaviour
     private enum Sub
     {
         Main,
-        HowToPlay
+        HowToPlay,
+        HowToPlay2
     }
     private void Awake()
     {
         transform.Find("howToPlaySubMenu").GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        transform.Find("howToPlaySubMenu2").GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
         transform.Find("mainSubMenu").GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
         transform.Find("mainSubMenu").transform.Find("playButton").GetComponent<Button_UI>().ClickFunc = () => Loader.Load(Loader.Scene.GameScene);
         transform.Find("mainSubMenu").transform.Find("playButton").GetComponent<Button_UI>().AddButtonSounds();
+
         transform.Find("mainSubMenu").transform.Find("quitButton").GetComponent<Button_UI>().ClickFunc = () => Application.Quit();
         transform.Find("mainSubMenu").transform.Find("quitButton").GetComponent<Button_UI>().AddButtonSounds();
+
         transform.Find("mainSubMenu").transform.Find("howToPlayButton").GetComponent<Button_UI>().ClickFunc = () => ShowSub(Sub.HowToPlay);
         transform.Find("mainSubMenu").transform.Find("howToPlayButton").GetComponent<Button_UI>().AddButtonSounds();
+
         transform.Find("howToPlaySubMenu").transform.Find("backButton").GetComponent<Button_UI>().ClickFunc = () => ShowSub(Sub.Main);
         transform.Find("howToPlaySubMenu").transform.Find("backButton").GetComponent<Button_UI>().AddButtonSounds();
+
+        transform.Find("howToPlaySubMenu").transform.Find("nextButton").GetComponent<Button_UI>().ClickFunc = () => ShowSub(Sub.HowToPlay2);
+        transform.Find("howToPlaySubMenu").transform.Find("nextButton").GetComponent<Button_UI>().AddButtonSounds();
+
+        transform.Find("howToPlaySubMenu2").transform.Find("previousButton").GetComponent<Button_UI>().ClickFunc = () => ShowSub(Sub.HowToPlay);
+        transform.Find("howToPlaySubMenu2").transform.Find("previousButton").GetComponent<Button_UI>().AddButtonSounds();
+
+        transform.Find("howToPlaySubMenu2").transform.Find("backButton").GetComponent<Button_UI>().ClickFunc = () => ShowSub(Sub.Main);
+        transform.Find("howToPlaySubMenu2").transform.Find("backButton").GetComponent<Button_UI>().AddButtonSounds();
+
+
         ShowSub(Sub.Main);
     }
 
@@ -30,6 +47,7 @@ public class MainMenuWindow : MonoBehaviour
     {
         transform.Find("mainSubMenu").gameObject.SetActive(false);
         transform.Find("howToPlaySubMenu").gameObject.SetActive(false);
+        transform.Find("howToPlaySubMenu2").gameObject.SetActive(false);
 
         switch (sub)
         {
@@ -38,6 +56,9 @@ public class MainMenuWindow : MonoBehaviour
                 break;
             case Sub.HowToPlay:
                 transform.Find("howToPlaySubMenu").gameObject.SetActive(true);
+                break;
+            case Sub.HowToPlay2:
+                transform.Find("howToPlaySubMenu2").gameObject.SetActive(true);
                 break;
         }
     }
